7f8cfdea538695145c578a78abc7bc8a776e3a90
Staticaliza <413584@gmail.com>
2026-04-16 17:14:12 -0400

    simplified endpoint

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
+# Imports
+import gradio
 import spaces
-import gradio as gr
 
-def test_api(payload: dict) -> dict:
-    return {"received": payload, "message": "Working", "status": "ok"}
+# Variables
+
+# Functions
+def endpoint(payload: dict) -> dict: return {"received": payload, "message": "Working", "status": "ok"}
 
 @spaces.GPU
 def _(): return None
 
-with gr.Blocks() as demo:
-    gr.Markdown("Test API")
-    gr.api(test_api, api_name="test_api")
+# Initialize
+with gradio.Blocks() as api:
+    gradio.api(endpoint, api_name="endpoint")
 
-demo.queue(default_concurrency_limit=16, max_size=256)
-demo.launch(ssr_mode=False)
+api.queue(default_concurrency_limit=16, max_size=256).launch(ssr_mode=False)
