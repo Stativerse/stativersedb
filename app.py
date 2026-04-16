19189067af9d1dbb974ddd58de34ebcb5ee27284
Staticaliza <413584@gmail.com>
2026-04-16 17:23:59 -0400

    added ping mechanic

--- app.py
+++ app.py
@@ -7,11 +7,14 @@ import spaces
 # Functions
 def endpoint(payload: dict) -> dict: return {"received": payload, "message": "Working", "status": "ok"}
 
-@spaces.GPU
-def _(): return None
+@spaces.GPU(size="large", duration=1)
+def ping():
+    print(f"SERVER | Space has been pinged. ☁️")
+    return
 
 # Initialize
 with gradio.Blocks() as api:
     gradio.api(endpoint, api_name="endpoint")
+    gradio.api(ping, api_name="ping")
 
 api.queue(default_concurrency_limit=16, max_size=256).launch(ssr_mode=False)
